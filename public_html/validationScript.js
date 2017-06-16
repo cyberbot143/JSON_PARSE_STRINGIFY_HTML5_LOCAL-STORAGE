bfcb9f5e80b7e7fe6e21001d73c073c4c619275e
S525777 <krishnasai952@gmail.com>
2017-06-16 10:37:01 -0400

    Read Table data

--- public_html/validationScript.js
+++ public_html/validationScript.js
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-function maxOccur() {
-    var arr1 = ['abcd', 'aabbkklkk', 'cccdd'];
+   var myarr = ['bbbcd', 'aaaaak', 'cdbjdd','hchgbbb'];
+   function maxOccur(myarr) {
+ 
+ var inputArray =myarr;
+    console.log(inputArray);
+    
     var testArray = [];
-    for (var a = 0; a < arr1.length; a++) {
-        var arr = arr1[a];
-        console.log(arr);
+    for (var a = 0; a < inputArray.length; a++) {
+        var arr = inputArray[a];
+         var max = 0;
         for (var i = 0; i < arr.length; i++) {
-            var count = 0;
-            var max = 0;
-
+           var count = 0; 
             for (var j = 0; j <= arr.length; j++) {
-
                 if (arr[j] == arr[i]) {
                     count++;
                     if (max < count)
@@ -24,111 +24,127 @@ function maxOccur() {
                     }
                 }
             }
+            
         }
         testArray.push(max);
-
-
     }
     console.log(testArray);
-    console.log(arr1[testArray.indexOf((Math.max.apply(Math, testArray)))]);
+    var max = testArray[0];
+    var maxIndex = 0;
+
+    for (var a = 1; a < testArray.length; a++) {
+        console.log(a);
+        if (testArray[a] > max) {
+
+            maxIndex = a;
+            max = testArray[a];
+        }
+    }
+    console.log(inputArray[maxIndex]);
+    return inputArray[maxIndex];
+}
+
+maxOccur(myarr);
+
+
+function displayTable(){
+    
+    
+        
+     var retrievedObject = localStorage.getItem("userList");
+     var parsedObject1 = JSON.parse(retrievedObject)
+     var html = "<table id='dataTable' border='1|1'>";
+    html+="<tr>";
+    for (var key in parsedObject1[0]){
+        html+="<th>"+key+"</th>";
+    }
+    html+="</tr>";
+      for (var i in parsedObject1 ) {
+      
+        html+="<tr id='parsedObject1[i]'>";
+        html+="<td>"+parsedObject1[i].Firstname+"</td>";
+        html+="<td>"+parsedObject1[i].lastname+"</td>";
+        html+="<td>"+parsedObject1[i].userName+"</td>";
+        html+="<td>"+parsedObject1[i].password+"</td>";
+        html+="<td>"+parsedObject1[i].email+"</td>";
+        html+="<td>"+parsedObject1[i].gender+"</td>";
+        html+="</tr>";
+    }
+    html+="</table>";
+if(localStorage.userList!==null){
+document.getElementById('display').innerHTML = html;
+}
+else{
+document.getElementById('display').innerHTML += html;
+}
 
 }
 
-function Validate(){
-    var user = { };
+var myArray = [];
+function Validate() {
+    var user = {};
     var flag = true;
+
     var fname = document.getElementById('firstName').value;
-    if(fname.length == 0)
+    if (fname.length == 0)
     {
-        document.getElementById('firstName').insertAdjacentHTML('afterend',"Please enter value");
+        document.getElementById('firstName').insertAdjacentHTML('afterend', "Please enter value");
         flag = false;
     }
-    
+
     var lname = document.getElementById('lastName').value;
-    if(lname.length == 0)
+    if (lname.length == 0)
     {
         console.log('Enter your last name');
         flag = false;
     }
     var uname = document.getElementById('userName').value;
-    if(uname.length == 0)
+    if (uname.length == 0)
     {
         console.log('Enter your user name');
         flag = false;
     }
     var pwd = document.getElementById('password').value;
-    if(pwd.length == 0)
+    if (pwd.length == 0)
     {
         console.log('Password Must not be empty');
         flag = false;
     }
     //radio button validation
     var x = "";
-    if(document.getElementById('male').checked){
-    console.log('you have checked male');
-    x = "male";
-    }
-    else if (document.getElementById('female').checked) {
+    if (document.getElementById('male').checked) {
+        console.log('you have checked male');
+        x = "male";
+    } else if (document.getElementById('female').checked) {
         console.log('you have checked female');
-        x= "female";
+        x = "female";
+    } else {
+        console.log('blah blah');
+        x = "";
+        flag = false;
     }
-    else{
-            console.log('blah blah');
-            x = "";
-            flag = false;
-    }         
-    if(flag==true)
+    if (flag == true)
     {
-    // Create an object and then push into local storage 
-    var user_arr= [];
-    user['Firstname'] = document.getElementById('firstName').value;
-    user['lastname'] = document.getElementById('lastName').value;
-    user['userName'] = document.getElementById('userName').value;
-    user['password'] = document.getElementById('password').value;
-    user['email'] = document.getElementById('email').value;
-    user['gender'] = x;
-    
-    localStorage.setItem('user',JSON.stringify(user));
-    var retrievedObject = localStorage.getItem('user');
-    console.log('retrievedObject: ', JSON.parse(retrievedObject));
-    
-        var myarray = Object.values(retrievedObject);
-        console.log(myarray);
+        // Create an object and then push into local storage 
+        user['Firstname'] = document.getElementById('firstName').value;
+        user['lastname'] = document.getElementById('lastName').value;
+        user['userName'] = document.getElementById('userName').value;
+        user['password'] = document.getElementById('password').value;
+        user['email'] = document.getElementById('email').value;
+        user['gender'] = x;
 
-
-
-    localStorage.setItem('firstName', JSON.stringify(user.Firstname));
-    localStorage.setItem('lastName', JSON.stringify(user.lastname));
-    localStorage.setItem('userName', JSON.stringify(user.userName));
-    localStorage.setItem('password', JSON.stringify(user.password));
-    localStorage.setItem('email', JSON.stringify(user.email));
-    localStorage.setItem('gender', JSON.stringify(user.gender));
-    
-   var retrievedArray = [];
-   
-   retrievedArray.push(localStorage.getItem('firstName'));
-    console.log('Data Retrived from local Storage');
-   
-   
+        myArray.push(user);
+        console.log(myArray);
+        localStorage.setItem('userList', JSON.stringify(myArray));
+        
+        displayTable();
+       
   document.getElementById("myForm").reset();
-  
- 
-  
-  console.log(myarray);
-  
-  var table = '';
-  var rows = user.length;
-  var cols = 6;
-   for(var r = 0;r<rows;r++){
-    table += '<tr>';
-    for(var c = 0;c<myarraylength;c++){
-        
-                table+= '<td>' + localStorage.getItem(c) +'</td>' 
-        
-    
+
+
             
-   }
- 
+
+        
+    }
 }
-}
-};
+;
