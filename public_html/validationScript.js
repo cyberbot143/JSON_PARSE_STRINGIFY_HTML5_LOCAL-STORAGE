4af668784c77258096a41dbfdde8358997a16b54
S525777 <krishnasai952@gmail.com>
2017-06-19 20:48:58 -0400

    As per Client Requirement Testing is done by test team

--- public_html/validationScript.js
+++ public_html/validationScript.js
@@ -3,48 +3,47 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//   var myarr = ['bbbcd', 'aaaaak', 'cdbjdd','hchgbbb'];
-//   function maxOccur(myarr) {
-// 
-// var inputArray =myarr;
-//    console.log(inputArray);
-//    
-//    var testArray = [];
-//    for (var a = 0; a < inputArray.length; a++) {
-//        var arr = inputArray[a];
-//         var max = 0;
-//        for (var i = 0; i < arr.length; i++) {
-//           var count = 0; 
-//            for (var j = 0; j <= arr.length; j++) {
-//                if (arr[j] == arr[i]) {
-//                    count++;
-//                    if (max < count)
-//                    {
-//                        max = count;
-//                    }
-//                }
-//            }
-//            
-//        }
-//        testArray.push(max);
-//    }
-//    console.log(testArray);
-//    var max = testArray[0];
-//    var maxIndex = 0;
-//
-//    for (var a = 1; a < testArray.length; a++) {
-//        console.log(a);
-//        if (testArray[a] > max) {
-//
-//            maxIndex = a;
-//            max = testArray[a];
-//        }
-//    }
-//    console.log(inputArray[maxIndex]);
-//    return inputArray[maxIndex];
-//}
-//
-//maxOccur(myarr);
+   var myarr = ['bbbcd', 'aaaaak', 'cdbjdd','hchgbbb'];
+   function maxOccur(myarr) {
+ 
+ var inputArray =myarr;
+    console.log(inputArray);
+    
+    var testArray = [];
+    for (var a = 0; a < inputArray.length; a++) {
+        var arr = inputArray[a];
+         var max = 0;
+        for (var i = 0; i < arr.length; i++) {
+           var count = 0; 
+            for (var j = 0; j <= arr.length; j++) {
+                if (arr[j] == arr[i]) {
+                    count++;
+                    if (max < count)
+                    {
+                        max = count;
+                    }
+                }
+            }
+            
+        }
+        testArray+=max;
+    }
+    console.log(testArray);
+    var max = testArray[0];
+    var maxIndex = 0;
+
+    for (var a = 1; a < testArray.length; a++) {
+        console.log(a);
+        if (testArray[a] > max) {
+             maxIndex = a;
+            max = testArray[a];
+        }
+    }
+    console.log(inputArray[maxIndex]);
+    return inputArray[maxIndex];
+}
+
+maxOccur(myarr);
 
 window.onload = function(){
     displayTable();
@@ -61,9 +60,24 @@ function Validate() {
    else{
       var  myArray = new Array();
    }
+   
+     var tags = document.getElementsByTagName("input");
     var flag = true;
-    //radio button validation
-      var x = "";
+    var store = document.querySelectorAll('input');
+    for (var val of store) {
+        var chk = document.getElementById("perr" + val.getAttribute('id'));
+        if (val.value == "") {
+            flag = false;
+            if (!chk) {
+                val.insertAdjacentHTML('afterend', '<p style="font-size: 16px;padding: 10px;background-color: #f44336; color: white;margin-bottom: 15px;margin-top: 5px; " data-valid id="perr' + val.getAttribute('id') + '">Please enter' + val.getAttribute('id') + ' field</p>');
+            }
+        } else {
+            if (chk) {
+                chk.remove();
+            }
+        }
+    }
+     var x = "";
     if (document.getElementById('male').checked) {
         document.getElementById('genderErr').innerHTML = ""; 
         x = "male";
@@ -75,41 +89,9 @@ function Validate() {
         x = "";
         flag = false;
     }
-    var fname = document.getElementById('firstName').value;
-    
-    if (fname.length == 0)
-    {
-        document.getElementById('firstNameErr').innerHTML = "Enter your first Name";
-        flag = false;
-        
-    }
+   
 
-    var lname = document.getElementById('lastName').value;
-    if (lname.length == 0)
-    {
-        document.getElementById('lastNameErr').innerHTML = "Enter your last name";
-        flag = false;
-    }
-    var uname = document.getElementById('userName').value;
-    if (uname.length == 0)
-    {
-       document.getElementById('userNameErr').innerHTML = "Enter your user name";
-        flag = false;
-    }
-    var pwd = document.getElementById('password').value;
-    if (pwd.length == 0)
-    {
-     document.getElementById('passwordErr').innerHTML = "Enter your password"; 
-        flag = false;
-    }
-    var mail = document.getElementById('email').value;
-    if(mail.length== 0){
-        document.getElementById('emailErr').innerHTML = "Enter your email"; 
-        flag = false;
-    }
-  
-
-  
+ 
     // if every field is filled
     if (flag == true)
     {
